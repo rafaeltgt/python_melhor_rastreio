dd0756c1fd0c654e05647f9cf72e89aee5e72025
Rafael Gaitan <rafael.tgt@gmail.com>
2020-11-16 14:48:40 -0300

    usage example

---
+++ usage_example.py
@@ -0,0 +1,7 @@
+from python_melhor_rastreio import track_melhor_rastreio
+
+print(track_melhor_rastreio('https://www.melhorrastreio.com.br/rastreio/18161300019484'))
+
+#or
+
+print(track_melhor_rastreio('18161300019484'))
